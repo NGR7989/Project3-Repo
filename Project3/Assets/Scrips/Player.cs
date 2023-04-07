f2f3d46859619e2c70e2a0f7241c1ec6c7e3159a
Narai Risser (RIT Student) <ngr7989@rit.edu>
2023-04-07 16:06:01 -0400

    Movement

--- Project3/Assets/Scrips/Player.cs
+++ Project3/Assets/Scrips/Player.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
+    [Tooltip("Time until looped movement can be considered")]
     [SerializeField] float timeUntilHold;
+    [Tooltip("The pause between each looped movement")]
     [SerializeField] float pauseForHoldDownTime;
     [SerializeField] int distancePerMove;
-    [Header("Checking")]
+    [Header("Collision")]
     [SerializeField] float checkOffset;
+    [SerializeField] float checkRadius;
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +30,17 @@ public class Player : MonoBehaviour
             // Only allow for one input at a time 
             float vertical = Input.GetAxis("Vertical");
             float horizontal = Input.GetAxis("Horizontal");
+            
 
             if (Mathf.Abs(vertical) > 0)
             {
                 // Vertical movement 
 
                 // Check if close enough under arbitrary value 
-                if(Mathf.Abs(heldVertical - vertical) < 0.01f)
+                if((heldVertical > 0 & vertical > 0) || (heldVertical < 0 & vertical < 0))
                 {
                     // Same value as before so treat as held down 
-
+                    bool canBeginLoop = true;
                     float pauseBeforeContinue = 0;
 
                     // Loop until held is done long enough 
@@ -43,21 +48,41 @@ public class Player : MonoBehaviour
                     {
                         // Check if pulled up during this time 
                         // Or if another key is pressed 
+                        float loopedValue = (int)Input.GetAxis("Vertical");
+                        if (Mathf.Abs(heldVertical - loopedValue) > 0.01f)
+                        {
+                            // Key up
+                            canBeginLoop = false;
+                        }
 
+                        pauseBeforeContinue += Time.deltaTime;
                         yield return null; 
                     }
 
                     float counter = 0;
 
                     // Continues movement loop 
-                    while(true)
+                    while(canBeginLoop)
                     {
                         // Check if pulled up during this time 
                         // Or if another key is pressed 
 
+                        float loopedValue = (int)Input.GetAxis("Vertical");
+                        if (Mathf.Abs(heldVertical - loopedValue) > 0.0001f)
+                        {
+                            // Key up
+                            canBeginLoop = false;
+                        }
+
                         if (counter >= pauseForHoldDownTime)
                         {
                             // Move player 
+                            Vector3 newPos = Vector3.up * distancePerMove * loopedValue;
+                            if (!IsColliding(newPos))
+                            {
+                                this.transform.position += newPos;
+                            }
+                            counter = 0;
                         }
                         else
                         {
@@ -73,22 +98,136 @@ public class Player : MonoBehaviour
                     // This is when the button is just pressed 
 
                     // New Value so treat like just pressed down 
-                    heldVertical = vertical;
-
-                    // Move player 
-                    
+                    if(vertical > 0)
+                    {
+                        if(!IsColliding(Vector3.up))
+                        {
+                            this.transform.position += Vector3.up * distancePerMove;
+                        }
+                    }
+                    else
+                    {
+                        if(!IsColliding(Vector3.down))
+                        {
+                            this.transform.position -= Vector3.up * distancePerMove;
+                        }
+                    }
                 }
             }
             else if(Mathf.Abs(horizontal) > 0)
             {
-                
+                // Horizontal movement 
+
+                // Check if close enough under arbitrary value 
+                if ((heldHorizontal > 0 & horizontal > 0) || (heldHorizontal < 0 & horizontal < 0))
+                {
+                    // Same value as before so treat as held down 
+                    bool canBeginLoop = true;
+                    float pauseBeforeContinue = 0;
+
+                    // Loop until held is done long enough 
+                    while (pauseBeforeContinue < timeUntilHold)
+                    {
+                        // Check if pulled up during this time 
+                        // Or if another key is pressed 
+                        float loopedValue = (int)Input.GetAxis("Horizontal");
+                        if (Mathf.Abs(heldHorizontal - loopedValue) > 0.01f)
+                        {
+                            // Key up
+                            canBeginLoop = false;
+                        }
+
+                        pauseBeforeContinue += Time.deltaTime;
+                        yield return null;
+                    }
+
+                    float counter = 0;
+
+                    // Continues movement loop 
+                    while (canBeginLoop)
+                    {
+                        // Check if pulled up during this time 
+                        // Or if another key is pressed 
+
+                        float loopedValue = (int)Input.GetAxis("Horizontal");
+                        if (Mathf.Abs(heldHorizontal - loopedValue) > 0.0001f)
+                        {
+                            // Key up
+                            canBeginLoop = false;
+                        }
+
+                        if (counter >= pauseForHoldDownTime)
+                        {
+                            // Move player 
+                            Vector3 newPos = Vector3.right * distancePerMove * loopedValue;
+                            if (!IsColliding(newPos))
+                            {
+                                this.transform.position += newPos;
+                            }
+                            counter = 0;
+                        }
+                        else
+                        {
+                            counter += Time.deltaTime;
+                        }
+
+                        yield return null;
+                    }
+
+                }
+                else
+                {
+                    // This is when the button is just pressed 
+
+                    // New Value so treat like just pressed down 
+                    if (horizontal > 0)
+                    {
+                        if (!IsColliding(Vector3.right))
+                        {
+                            this.transform.position += Vector3.right * distancePerMove;
+                        }
+                    }
+                    else
+                    {
+                        if (!IsColliding(Vector3.left))
+                        {
+                            this.transform.position -= Vector3.right * distancePerMove;
+                        }
+                    }
+                }
             }
 
-
-            // Logic for on key up 
-
+            heldVertical = vertical;
+            heldHorizontal = horizontal;
 
             yield return null;
         }
+    }
+
+    private bool IsColliding(Vector2 offset)
+    {
+        return Physics2D.OverlapCircle((Vector2)this.transform.position + offset, checkRadius)!= null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawCollision(Vector3.up);
+        DrawCollision(Vector3.down);
+        DrawCollision(Vector3.right);
+        DrawCollision(Vector3.left);
+    }
+
+    private void DrawCollision(Vector3 offset)
+    {
+        if (IsColliding(offset))
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.white;
+        }
+
+        Gizmos.DrawWireSphere(this.transform.position + offset * checkOffset, checkRadius);
     }
 }
