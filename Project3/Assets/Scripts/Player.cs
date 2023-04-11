b9ae5c7f57120489fa256f240ebf9a775fc820ed
Narai Risser <89745834+NGR7989@users.noreply.github.com>
2023-04-11 16:50:33 -0400

    Interaction code

--- Project3/Assets/Scripts/Player.cs
+++ Project3/Assets/Scripts/Player.cs
@@ -11,18 +11,35 @@ public class Player : MonoBehaviour
     [Header("Collision")]
     [SerializeField] float checkOffset;
     [SerializeField] float checkRadius;
+    [Header("Interaction")]
+    [SerializeField] KeyCode interactionKey;
 
     private Vector3 currentDir = Vector3.zero;
+    private Vector3 interactionDir = Vector3.down; // For interaction 
 
     // Start is called before the first frame update
     void Start()
     {
+        // Begin movement coroutines 
         StartCoroutine(MoveDir(Vector3.up, KeyCode.W));
         StartCoroutine(MoveDir(Vector3.down, KeyCode.S));
         StartCoroutine(MoveDir(Vector3.right, KeyCode.D));
         StartCoroutine(MoveDir(Vector3.left, KeyCode.A));
     }
 
+    private void Update()
+    {
+        Interaction();
+    }
+
+    #region Movement  
+    /// <summary>
+    /// The coroutine the overrides a direction to be moved
+    /// and continues to move the player after a pause 
+    /// </summary>
+    /// <param name="dir"></param>
+    /// <param name="key"></param>
+    /// <returns></returns>
     private IEnumerator MoveDir(Vector3 dir, KeyCode key)
     {
         float holdDownTime = 0;
@@ -32,6 +49,7 @@ public class Player : MonoBehaviour
             if(Input.GetKeyDown(key))
             {
                 currentDir = dir;
+                interactionDir = dir;
 
                 // Check before moving 
                 if (!IsColliding(dir * distancePerMove))
@@ -75,19 +93,10 @@ public class Player : MonoBehaviour
         }
     }
 
-    private bool IsColliding(Vector2 offset)
-    {
-        return Physics2D.OverlapCircle((Vector2)this.transform.position + offset, checkRadius)!= null;
-    }
-
-    private void OnDrawGizmos()
-    {
-        DrawCollision(Vector3.up);
-        DrawCollision(Vector3.down);
-        DrawCollision(Vector3.right);
-        DrawCollision(Vector3.left);
-    }
-
+    /// <summary>
+    /// Used to visualize an anticipated collision 
+    /// </summary>
+    /// <param name="offset"></param>
     private void DrawCollision(Vector3 offset)
     {
         if (IsColliding(offset))
@@ -101,4 +110,62 @@ public class Player : MonoBehaviour
 
         Gizmos.DrawWireSphere(this.transform.position + offset * checkOffset, checkRadius);
     }
+    #endregion
+
+    /// <summary>
+    /// Runs the logic of whether the player is to 
+    /// interact with an object in the direction
+    /// they last moved 
+    /// </summary>
+    private void Interaction()
+    {
+        if(Input.GetKeyDown(interactionKey))
+        {
+            // Try for collision which is interactable 
+            if(IsColliding(interactionDir))
+            {
+                // Try to get interactable 
+                Interactable interactable = GetInteractable(interactionDir);
+
+                if(interactable != null)
+                {
+                    // Activates interactable if possible 
+                    interactable.Interact();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Checks whether if in a certain direction the player
+    /// is colliding with an object 
+    /// </summary>
+    /// <param name="offset"></param>
+    /// <returns></returns>
+    private bool IsColliding(Vector2 offset)
+    {
+        return Physics2D.OverlapCircle((Vector2)this.transform.position + offset, checkRadius) != null;
+    }
+
+    /// <summary>
+    /// Get an interactable if possible towards the 
+    /// last moved direction 
+    /// </summary>
+    /// <param name="offset"></param>
+    /// <returns></returns>
+    private Interactable GetInteractable(Vector2 offset)
+    {
+        Collider2D collider = Physics2D.OverlapCircle((Vector2)this.transform.position + offset, checkRadius);
+        return collider == null ? null : collider.GetComponent<Interactable>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawCollision(Vector3.up);
+        DrawCollision(Vector3.down);
+        DrawCollision(Vector3.right);
+        DrawCollision(Vector3.left);
+    }
+
+  
 }
