8772aad7a7bb58f8b302e0925b2f6eb3ef65a6fd
Narai Risser (RIT Student) <ngr7989@rit.edu>
2023-04-10 22:08:03 -0400

    Greatly simplified movement

--- Project3/Assets/Scripts/Player.cs
+++ Project3/Assets/Scripts/Player.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Movement")]
-    [Tooltip("Time until looped movement can be considered")]
-    [SerializeField] float timeUntilHold;
     [Tooltip("The pause between each looped movement")]
     [SerializeField] float pauseForHoldDownTime;
     [SerializeField] int distancePerMove;
@@ -14,191 +12,64 @@ public class Player : MonoBehaviour
     [SerializeField] float checkOffset;
     [SerializeField] float checkRadius;
 
+    private Vector3 currentDir = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(MoveCo());
+        StartCoroutine(MoveDir(Vector3.up, KeyCode.W));
+        StartCoroutine(MoveDir(Vector3.down, KeyCode.S));
+        StartCoroutine(MoveDir(Vector3.right, KeyCode.D));
+        StartCoroutine(MoveDir(Vector3.left, KeyCode.A));
     }
 
-    private IEnumerator MoveCo()
+    private IEnumerator MoveDir(Vector3 dir, KeyCode key)
     {
-        float heldVertical = 0;
-        float heldHorizontal = 0;
+        float holdDownTime = 0;
 
         while(true)
         {
-            // Only allow for one input at a time 
-            float vertical = Input.GetAxis("Vertical");
-            float horizontal = Input.GetAxis("Horizontal");
-            
-
-            if (Mathf.Abs(vertical) > 0)
+            if(Input.GetKeyDown(key))
             {
-                // Vertical movement 
+                currentDir = dir;
 
-                // Check if close enough under arbitrary value 
-                if((heldVertical > 0 & vertical > 0) || (heldVertical < 0 & vertical < 0))
+                // Check before moving 
+                if (!IsColliding(dir * distancePerMove))
                 {
-                    // Same value as before so treat as held down 
-                    bool canBeginLoop = true;
-                    float pauseBeforeContinue = 0;
-
-                    // Loop until held is done long enough 
-                    while(pauseBeforeContinue < timeUntilHold)
-                    {
-                        // Check if pulled up during this time 
-                        // Or if another key is pressed 
-                        float loopedValue = (int)Input.GetAxis("Vertical");
-                        if (Mathf.Abs(heldVertical - loopedValue) > 0.01f)
-                        {
-                            // Key up
-                            canBeginLoop = false;
-                        }
-
-                        pauseBeforeContinue += Time.deltaTime;
-                        yield return null; 
-                    }
-
-                    float counter = 0;
-
-                    // Continues movement loop 
-                    while(canBeginLoop)
-                    {
-                        // Check if pulled up during this time 
-                        // Or if another key is pressed 
-
-                        float loopedValue = (int)Input.GetAxis("Vertical");
-                        if (Mathf.Abs(heldVertical - loopedValue) > 0.0001f)
-                        {
-                            // Key up
-                            canBeginLoop = false;
-                        }
-
-                        if (counter >= pauseForHoldDownTime)
-                        {
-                            // Move player 
-                            Vector3 newPos = Vector3.up * distancePerMove * loopedValue;
-                            if (!IsColliding(newPos))
-                            {
-                                this.transform.position += newPos;
-                            }
-                            counter = 0;
-                        }
-                        else
-                        {
-                            counter += Time.deltaTime;
-                        }
-
-                        yield return null;
-                    }
-                    
-                }
-                else
-                {
-                    // This is when the button is just pressed 
-
-                    // New Value so treat like just pressed down 
-                    if(vertical > 0)
-                    {
-                        if(!IsColliding(Vector3.up))
-                        {
-                            this.transform.position += Vector3.up * distancePerMove;
-                        }
-                    }
-                    else
-                    {
-                        if(!IsColliding(Vector3.down))
-                        {
-                            this.transform.position -= Vector3.up * distancePerMove;
-                        }
-                    }
-                }
-            }
-            else if(Mathf.Abs(horizontal) > 0)
-            {
-                // Horizontal movement 
-
-                // Check if close enough under arbitrary value 
-                if ((heldHorizontal > 0 & horizontal > 0) || (heldHorizontal < 0 & horizontal < 0))
-                {
-                    // Same value as before so treat as held down 
-                    bool canBeginLoop = true;
-                    float pauseBeforeContinue = 0;
-
-                    // Loop until held is done long enough 
-                    while (pauseBeforeContinue < timeUntilHold)
-                    {
-                        // Check if pulled up during this time 
-                        // Or if another key is pressed 
-                        float loopedValue = (int)Input.GetAxis("Horizontal");
-                        if (Mathf.Abs(heldHorizontal - loopedValue) > 0.01f)
-                        {
-                            // Key up
-                            canBeginLoop = false;
-                        }
-
-                        pauseBeforeContinue += Time.deltaTime;
-                        yield return null;
-                    }
-
-                    float counter = 0;
-
-                    // Continues movement loop 
-                    while (canBeginLoop)
-                    {
-                        // Check if pulled up during this time 
-                        // Or if another key is pressed 
-
-                        float loopedValue = (int)Input.GetAxis("Horizontal");
-                        if (Mathf.Abs(heldHorizontal - loopedValue) > 0.0001f)
-                        {
-                            // Key up
-                            canBeginLoop = false;
-                        }
-
-                        if (counter >= pauseForHoldDownTime)
-                        {
-                            // Move player 
-                            Vector3 newPos = Vector3.right * distancePerMove * loopedValue;
-                            if (!IsColliding(newPos))
-                            {
-                                this.transform.position += newPos;
-                            }
-                            counter = 0;
-                        }
-                        else
-                        {
-                            counter += Time.deltaTime;
-                        }
-
-                        yield return null;
-                    }
-
-                }
-                else
-                {
-                    // This is when the button is just pressed 
-
-                    // New Value so treat like just pressed down 
-                    if (horizontal > 0)
-                    {
-                        if (!IsColliding(Vector3.right))
-                        {
-                            this.transform.position += Vector3.right * distancePerMove;
-                        }
-                    }
-                    else
-                    {
-                        if (!IsColliding(Vector3.left))
-                        {
-                            this.transform.position -= Vector3.right * distancePerMove;
-                        }
-                    }
+                    this.transform.position += dir * distancePerMove;
                 }
             }
 
-            heldVertical = vertical;
-            heldHorizontal = horizontal;
+            if(Input.GetKey(key) && currentDir.Equals(dir))
+            {
+                // Repeat while the same direction 
+                holdDownTime += Time.deltaTime;
+
+                if(holdDownTime >= pauseForHoldDownTime)
+                {
+                    holdDownTime = 0;
+
+                    // Check before moving 
+                    if(!IsColliding(dir * distancePerMove))
+                    {
+                        this.transform.position += dir * distancePerMove;
+                    }
+                }
+            }
+            else if(Input.GetKeyUp(key))
+            {
+                holdDownTime = 0;
+
+                if(currentDir.Equals(dir))
+                {
+                    // Resets if no other key is pressed 
+                    currentDir = Vector3.zero;
+                }
+            }
+            else
+            {
+                holdDownTime = 0;
+            }
 
             yield return null;
         }
